0a70cf1bf6a9cc09dab265fd6dc61f705f6c1cff
Xin Fang <fangxin1996@gmail.com>
2023-12-22 00:17:49 -0500

    create frontend

---
+++ big-event-frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Hello, Vue!</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'App',
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
